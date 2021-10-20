cfc2582ca446f76f8e22b02c1552196a9c30f5d0
Cordero Ebberhart <corderoebberhart@rocketmortgage.com>
2021-10-20 08:19:45 -0400

    Solving for the radius

---
+++ CircleLab10/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleLab10
+{
+    class Circle
+    {
+        public double radius { get; set; }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override string ToString()
+        {
+            string output = $"Circuference: {CalculateCircumference(radius)}\n";
+            output += $"Area: {CalculateArea(radius)}\n";
+            return output;
+        }
+        public double CalculateCircumference(double radius)
+        {
+            Console.WriteLine(radius);
+            double cirCirc = 2 * Math.PI * radius;
+            return cirCirc;
+        }
+        public double CalculateArea(double radius)
+        {
+            Console.WriteLine(radius);
+            double cirArea = 2 * Math.PI * radius;
+            return cirArea;
+        }
+
+    }
+}
---
+++ CircleLab10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CircleLab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome the user
+            //Prompt the user to enter a radius
+            Console.WriteLine("Welcome to the Circle Tester");
+            Console.WriteLine("Enter a radius");
+            double radius = double.Parse(Console.ReadLine());
+            Circle circle = new Circle(radius);
+
+            double circumference = circle.CalculateCircumference(radius);
+            Console.WriteLine("Circumference: " + circumference);
+            double area = circle.CalculateArea(radius);
+            Console.WriteLine("Area: " + area);
+            
+            
+        }
+         
+    }
+}
